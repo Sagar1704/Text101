e8e3cdb09b41302396f6204caf368bde50b3a961
Sagar Deshpande 🐼 <sagad@amazon.com>
2020-05-10 11:04:05 -0700

    All the changes related Text101 udemy unity course.

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    // SerializeField - It is available in the Unity inspector
+    [SerializeField] Text textStory;
+    [SerializeField] Text textTitle;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Use this for initialization
+    void Start () {
+        this.StartGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            this.SetState(startingState.GetNextStates()[0]);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            this.SetState(startingState.GetNextStates()[1]);
+        }
+        //else
+        //{
+        //    StartGame();
+        //}
+    }
+
+    private void StartGame() {
+        this.SetState(startingState);
+    }
+
+    void SetState(State currentState) {
+        state = currentState;
+        textTitle.text = currentState.GetStateTitle();
+        textStory.text = currentState.GetStateStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The scriptable object does not need to be attached to the GameObject
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+    [TextArea(10, 14)] [SerializeField] string storyText;
+    [TextArea(10, 14)] [SerializeField] string storyTitle;
+
+    [SerializeField] State[] nextStates;
+
+    public string GetStateTitle() {
+        return storyTitle;
+    }
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+}
